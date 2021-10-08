d0d05b6c32110261b307d73d70ff3d927df645b2
J <oncwnuAtbq7RWXrDbsrMJw4vPdjA@git.weixin.qq.com>
2021-10-08 11:40:23 +0800

    更新

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <Person></Person>    
+  </div>
+</template>
+
+<script>
+import Person from './Person'
+
+export default {
+    name:'App',
+    components:{
+        Person
+    }
+}
+</script>
---
+++ 19_单文件组件/Person.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="demo">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <button @click="showName">点击显示姓名</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Person",
+  data() {
+    return {
+      name: "张三",
+      age: 18,
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.name);
+    },
+  },
+};
+</script>
+
+<style>
+.demo {
+  background-color: orange;
+}
+</style>
